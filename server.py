bb5fb00859dd3aee6fdb7142b46af01109493be8
dm39910 <75129276+dm39910@users.noreply.github.com>
2025-08-01 08:41:39 +0200

    server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+from flask_cors import CORS
 from flask import Flask, request, jsonify
 import csv
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/unesi", methods=["POST"])
 def unesi():
